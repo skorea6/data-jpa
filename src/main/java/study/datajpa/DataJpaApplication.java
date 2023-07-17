114d8b39c395eab43eb1ed7334e1fcce9ac1e7d8
talk105 <talk105@naver.com>
2023-07-17 11:25:10 +0900

    Initial commit

---
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -0,0 +1,24 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing // 생성시, 업데이트시 시간에 대한 것 (Auditing)
+@SpringBootApplication
+public class DataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	@Bean // 실무에서는 세션 정보나, 스프링 시큐리티 로그인 정보에서 ID를 받음 (Auditing)
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+}
